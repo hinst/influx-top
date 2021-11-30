6e3a32d79102f258e96ca4b101004864cfe8886f
Alexander Savinykh <asavinykh@nbrx.de>
2021-11-30 13:01:01 +0100

    Added class RowReader

---
+++ src/RowReader.ts
@@ -0,0 +1,28 @@
+import { FluxTableMetaData, QueryApi } from "@influxdata/influxdb-client";
+
+export abstract class RowReader<T> {
+    protected resolve: (rowReader: T) => void =
+        () => { throw new Error('Should never happen') };
+    private reject: (error: Error) => void =
+        () => { throw new Error('Should never happen') };
+    readonly promise: Promise<T>;
+
+    constructor() {
+        this.promise = new Promise((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+        });
+    }
+
+    abstract next(row: string[], tableMeta: FluxTableMetaData): void;
+    abstract complete(): void;
+
+    error(error: Error) {
+        this.reject(error);
+    }
+
+    async read(queryApi: QueryApi, query: string) {
+        queryApi.queryRows(query, this);
+        return await this.promise;
+    }
+}
